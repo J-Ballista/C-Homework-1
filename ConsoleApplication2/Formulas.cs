f1038fb89b2f86c601301183499cf8cddca16598
Javier <Javier@DESKTOP-SNMNTFT>
2016-10-10 19:01:39 -0400

    test

---
+++ ConsoleApplication2/Formulas.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Homework1_3;
+
+namespace Homework1_4 // YAY!!!
+{
+    public class MainFormulas
+    {
+        static void Main ()
+        {
+            int x;
+            string Input;
+            bool Parsed;
+            bool Exit = false;
+
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Please choose from the following : ");
+                    Console.WriteLine();
+                    Console.WriteLine("1 : Multiples Table Generator");
+                    Console.WriteLine("2 : Max Equation");
+                    Console.WriteLine("3 : Min Equation");
+                    Console.WriteLine("4 : Average Equation");
+                    Console.WriteLine("5 : Exit Application");
+                    Console.WriteLine();
+                    Console.Write("Enter Here : ");
+                    Input = Console.ReadLine();
+                    Parsed = Int32.TryParse(Input, out x);
+
+                    if (Parsed)
+                    {
+                        if (x == 1)
+                        {
+                            var multTable = new MultiplyingTable();
+                            multTable.CreateTable();
+                        }
+                        else if (x == 2)
+                        {
+                            var getMax = new MainFormulas();
+                            getMax.Max();
+                        }
+                        else if (x == 3)
+                        {
+                            var getMin = new MainFormulas();
+                            getMin.Min();
+                        }
+                        else if (x == 4)
+                        {
+                            var getAvg = new MainFormulas();
+                            getAvg.Average();
+                        }
+                        else if (x == 5)
+                        {
+                            Exit = true;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Incorrect Input Please Try Again");
+                        Console.WriteLine();
+                    }
+                }
+                while (!Parsed);
+            }
+            while (Exit == false);
+
+            Console.WriteLine("Thanks for using this application");
+            Console.ReadLine();
+        }
+
+        public void Max ()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Insert A series of Numbers separated by Spaces");
+            string[] maxInputs = Console.ReadLine().Split();
+            bool checkInput;
+            int z;
+            int oldz = 0;
+
+            foreach(string item in maxInputs)
+            {
+                checkInput = int.TryParse(item, out z);
+                
+                if (z > oldz)
+                {
+                    oldz = z;
+                }
+                
+            }
+            Console.WriteLine("Max Number : " + oldz);
+            Console.ReadLine();
+        }
+
+        public void Min()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Insert A series of Numbers separated by Spaces");
+            string[] maxInputs = Console.ReadLine().Split();
+            bool checkInput;
+            int z;
+            int oldz = 1000000;
+
+            foreach (string item in maxInputs)
+            {
+                checkInput = int.TryParse(item, out z);
+
+                if (z < oldz)
+                {
+                    oldz = z;
+                }
+
+            }
+            Console.WriteLine("Min Number : " + oldz);
+            Console.ReadLine();
+        }
+
+        public void Average()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Insert A series of Numbers separated by Spaces");
+            string[] avgInputs = Console.ReadLine().Split();
+            bool checkInput;
+            decimal z = 0;
+            decimal totalz = 0;
+            decimal countz = 0;
+
+            foreach (string input in avgInputs)
+            {
+                checkInput = Decimal.TryParse(input, out z);
+                totalz += z;
+                countz++;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Average of values is : " + totalz / countz);
+        }
+    }
+}
---
+++ ConsoleApplication2/Tables.cs
@@ -0,0 +1,94 @@
+﻿using Homework1_4;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_3
+{
+    public class MultiplyingTable
+    {
+        public void CreateTable()
+        {
+
+            int x;
+            int y;
+            string Input;
+            string Input2;
+            bool Parsed;
+            bool Parsed2;
+            bool Restart = false;
+
+            Console.WriteLine();
+            Console.WriteLine("Multiples Table Generator");
+
+            do
+            {
+                Console.Write("Please Insert Multiple : ");
+                Input = Console.ReadLine();
+                Parsed = Int32.TryParse(Input, out x);
+
+                if (Parsed)
+                {
+                    MultTable(x);
+
+                    do
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("(1) Return to Main Menu or (2) To Quit?");
+                        Input2 = Console.ReadLine();
+                        Parsed2 = Int32.TryParse(Input2, out y);
+
+                        if (Parsed2)
+                        {
+                            if (y == 1)
+                            {
+                                Restart = true;
+                            }
+                            else if (y == 2)
+                            {
+                                closeApp();
+                            }
+                            else
+                            {
+                                Console.WriteLine();
+                                Console.WriteLine("Incorrect Input");
+                                Console.WriteLine();
+                            }
+                        }  
+                    }
+                    while (!Parsed2);
+                }
+                else if (!Parsed)
+                {
+                    Console.WriteLine("Incorrect Input, Please only input a number");
+                }
+            }
+            while (Restart != true);
+
+            Console.WriteLine();
+            Console.WriteLine("Restarting Application");
+            Console.WriteLine();
+        }
+
+        static void MultTable(int x)
+        {
+            Console.WriteLine();
+
+            for(int i = 1; i < x+1; i++)
+            {
+                for(int j = 1; j < x+1; j++)
+                {
+                    Console.Write(i*j + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void closeApp()
+        {
+            Environment.Exit(1);
+        }
+    }
+}
